a8d9e1f152b5c28e5501b79c055d5b837d53cd27
Singade <Singadeganesh@gmail.com>
2019-11-25 22:10:31 +0530

    shell file first commit

---
+++ system_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "this system define variables shell script file"
+echo 'BASH=' $BASH
+echo 'BASHH_VERSION=' $BASH_VERSION
+echo 'HOSTNAME=' $HOSTNAME
+echo 'TERM=' $TERM
+echo 'SHELL=' $SHELL
+echo 'HISTSIZE=' $HISTSIZE
+
