4f6a25714a1906d784b0b915e1309e796566b8ea
Singade <Singadeganesh@gmail.com>
2019-12-03 21:56:43 +0530

    commited arthameticopes file

---
+++ arthimiteci_Operation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#date:03/12/2019
+echo 'Arthametci operation addation of two number'
+echo please enter any two numbers
+read a1 b1
+echo "addition of two numbers:" `expr $a1 + $b1`
+
+
+
